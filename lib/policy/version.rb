24e8e2e04100b9be21bd6528bcc7bad943303653
Andrew Kozin <andrew.kozin@gmail.com>
2015-03-12 16:00:26 +0300

    [VERSION] 1.0.1

--- lib/policy/version.rb
+++ lib/policy/version.rb
@@ -4,6 +4,6 @@ module Policy
 
   # The semantic version of the module.
   # @see http://semver.org/ Semantic versioning 2.0
-  VERSION = "1.0.0".freeze
+  VERSION = "1.0.1".freeze
 
 end # module Hexx
