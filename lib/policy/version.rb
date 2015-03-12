9cc156a81fecce351c13c38933a88b52bfbe103c
Andrew Kozin <andrew.kozin@gmail.com>
2015-03-12 17:14:08 +0300

    [VERSION] 1.0.4
    Fixed bugs from 'hexx-suit' dependency.

--- lib/policy/version.rb
+++ lib/policy/version.rb
@@ -4,6 +4,6 @@ module Policy
 
   # The semantic version of the module.
   # @see http://semver.org/ Semantic versioning 2.0
-  VERSION = "1.0.3".freeze
+  VERSION = "1.0.4".freeze
 
 end # module Hexx
